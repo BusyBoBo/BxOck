f16c1a9d8352b5ff07fc8e08006a886cdbcc0977
mqz <mqz1228@163.com>
2017-11-30 10:53:43 +0800

    新建

---
+++ BxOck/BxOck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BxOck
+//
+//  Created by BusyBoBo on 2017/11/30.
+//  Copyright © 2017年 BusyBoBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BxOck/BxOck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BxOck
+//
+//  Created by BusyBoBo on 2017/11/30.
+//  Copyright © 2017年 BusyBoBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
